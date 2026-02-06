49ac9d1f4a1f817f1cbe7b4a75d71ec0d5dfe7ac
AMT94-star <aslihan.taskin94@gmail.com>
2026-02-06 11:13:39 +0100

    lagt till form i balance sidan samt insättnings test i js

--- tests/balance.spec.js
+++ tests/balance.spec.js
@@ -9,4 +9,16 @@ test('Saldo should start at 0', async ({page}) => {
     await page.goto('http://localhost:8080/balance');
     const saldoText = await page.locator('span#balance').textContent();
     expect(saldoText).toBe('0');
+});
+
+test('Insättning uppdates balance', async ({page}) => {
+    await page.goto("http://localhost:8080/balance");
+
+    //klicka på insättning och fyll i 10
+    await page.fill('#deposit-input', '10');
+    await page.fill('#deposit-button');
+
+    const saldoText = await page.locator('span#balance').textContent();
+    expect(saldoText).toBe('10');
+
 })
